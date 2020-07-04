d9178ecbc96b262835bd4dfad047967f5e38e81c
ASHWINI PAGAR <ashwinipagar181996@gmail.com>
2020-07-04 17:20:22 +0530

    To the top functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,17 @@ var button = document.getElementById('show-popup');
 var close = document.getElementById('close');
 var popup = document.getElementById('popup');
 
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
 // Firefox 1.0+
 var isFirefox = typeof InstallTrigger !== 'undefined';
 
 // Safari 3.0+ "[object HTMLElementConstructor]" 
 var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
 
 // Edge 20+
 var isEdge = !isIE && !!window.StyleMedia;
@@ -15,22 +21,37 @@ var isEdge = !isIE && !!window.StyleMedia;
 // Chrome 1 - 79
 var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
+// Edge (based on chromium) detection
+var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+// Add and removve popup
+close.addEventListener("click", function(e){
+  popup.classList.add("close-popup");
+});
+
 button.addEventListener("click", function(e){
 	e.preventDefault();
 	popup.classList.add('display-popup');
+	popup.classList.remove('close-popup');
   if(isChrome || isSafari) {
 	  popup.classList.add("right-popup");    
   } else if(isFirefox) {
 	  popup.classList.add("left-popup");    
-	} else if(Edge) {
+	} else if(isEdge) {
 	  popup.classList.add("center-popup");    
 	} else {
 	  popup.classList.add("center-popup");
 	  var popupText = document.getElementById('popup-text');
-	  popupText.value = 'Browser Not Supported';   
+	  popupText.innerHTML = 'Browser Not Supported  :(';   
 	}
 });
 
-close.addEventListener("click", function(e){
-  popup.classList.add("close-popup");
+// Scroll to top functionality
+var top = document.getElementById('top');
+
+top.addEventListener("click", function(e){
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
 });
